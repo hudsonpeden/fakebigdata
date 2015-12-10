ebda16bd7dc9ce809f0829256067393f03719e8c
hudsonpeden <hudsonpeden@gmail.com>
2015-12-10 09:41:05 -0600

    Finalized for db usage

--- server.js
+++ server.js
@@ -1,43 +1,20 @@
 var express = require('express');
 var app = express();
-var mongojs = require('mongojs');
-// var db = mongojs('test', ['factorialinsert', 'primeInsert', 'fibInsert']);
-var db = mongojs('test', ['fibInsert']);
 var bodyParser = require('body-parser');
-var bigInt = require("big-integer");
+
+var mongojs = require('mongojs');
+var db = mongojs('fakebigdata', ['factorial', 'fibonacci', 'prime']);
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 //FACTORIAL
-// app.get('/factorialinsert', function (req, res){
-// 	console.log("I recieved a GET request");
-// });
-//
-// app.get('/factorialinsert/:id', function (req, res) {
-//
-// 	var id = req.params.id;
-// 	console.log("ID is: " + id);
-//
-// 	db.factorialinsert.findOne({
-// 		seriesNum: id
-// 	}, function(err, doc){
-// 		console.log("DOC is: " + doc.value);
-// 		res.json(doc.value);
-// 	});
-// });
-
-//FIBONACCI
-app.get('/fibInsert', function (req, res){
-	console.log("I recieved a GET request");
-});
-
-app.get('/fibInsert/:id', function (req, res) {
+app.get('/factorial/:id', function (req, res) {
 
 	var id = req.params.id;
 	console.log("ID is: " + id);
 
-	db.fibInsert.findOne({
+	db.factorial.findOne({
 		seriesNum: id
 	}, function(err, doc){
 		console.log("DOC is: " + doc.value);
@@ -45,28 +22,35 @@ app.get('/fibInsert/:id', function (req, res) {
 	});
 });
 
-// //PRIME
-// app.get('/primeInsert', function (req, res){
-// 	console.log("I recieved a GET request");
-// });
-//
-// app.get('/primeInsert/:id', function (req, res) {
-//
-// 	var id = req.params.id;
-// 	console.log("ID is: " + id);
-//
-// 	db.primeInsert.findOne({
-// 		seriesNum: id
-// 	}, function(err, doc){
-// 		console.log("DOC is: " + doc.value);
-// 		res.json(doc.value);
-// 	});
-// });
+//FIBONACCI
+app.get('/fibonacci/:id', function (req, res) {
+
+	var id = req.params.id;
+	console.log("ID is: " + id);
+
+	db.fibonacci.findOne({
+		seriesNum: id
+	}, function(err, doc){
+		console.log("DOC is: " + doc.value);
+		res.json(doc.value);
+	});
+});
+
+//PRIME
+app.get('/prime/:id', function (req, res) {
+
+	var id = req.params.id;
+	console.log("ID is: " + id);
+
+	db.prime.findOne({
+		seriesNum: id
+	}, function(err, doc){
+		console.log("DOC is: " + doc.value);
+		res.json(doc.value);
+	});
+});
 
 
 
-
-
-
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(8081);
+console.log("Server running on port 8081");
